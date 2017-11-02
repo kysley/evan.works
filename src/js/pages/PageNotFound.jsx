1b243733b2e8fc4ef7e56c4d5ce714aeb89b72f7
Evan Kysley <evan@kysley.com>
2017-11-02 15:44:11 -0400

    appease the linter

--- src/js/pages/PageNotFound.jsx
+++ src/js/pages/PageNotFound.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default class PageNotFound extends React.Component {
-	componentDidMount() {
-		document.title = 'NOT FOUND! EVAN WORKS'
-	}
+  componentDidMount() {
+    document.title = 'NOT FOUND! EVAN WORKS'
+  }
 
-	render() {
-		return (
-			<div className="notfoundsendhelp">
-				<Link to="/"><span>back >:( </span></Link>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="notfoundsendhelp">
+        <Link to="/"><span>{'back >:('}</span></Link>
+      </div>
+    )
+  }
 }
