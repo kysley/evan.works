e3c4e3c223e150dff8dee6aa3339a249634b7bec
Evan Kysley <evan@kysley.com>
2017-11-02 15:45:29 -0400

    it really isnt supposed to be responsive

--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -1,29 +1,50 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import Banner from '../components/Banner'
 import Item from '../components/Item'
-import allWorks, { moreWork } from '../utils/works'
+import allWorks from '../utils/works'
 
 export default class Index extends React.Component {
-	componentDidMount() {
-		document.title = 'EVAN WORKS'
-	}
-	
-	render() {
-		return (
-			<div>
-				<Banner
-					first="EVAN"
-					last="KYSLEY"
-				/>
-				{allWorks.map(work => (
-					<Item
-						work={work}
-						key={work.order}
-					/>
-				))}
-			</div>
-		)
-	}
+  constructor() {
+    super()
+
+    this.state = {
+      isResizing: false,
+    }
+  }
+
+  componentDidMount() {
+    document.title = 'EVAN WORKS'
+    window.addEventListener('resize', this.resize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize)
+  }
+
+  resize = () => {
+    clearTimeout(window.resizingFinished)
+    if (!this.state.isResizing) this.setState({ isResizing: true })
+    window.resizingFinished = setTimeout(() => {
+      this.setState({ isResizing: false })
+    }, 250)
+  }
+
+  render() {
+    return (
+      <div>
+        <Banner
+          first="EVAN"
+          last="KYSLEY"
+        />
+        {allWorks.map(work => (
+          <Item
+            work={work}
+            key={work.order}
+          />
+        ))}
+        { this.state.isResizing && <h1 className="NOT-RESPONSIVE">NOT RESPONSIVE</h1> }
+      </div>
+    )
+  }
 }
